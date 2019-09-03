c4ee2b45ca4f1239384cee822d7ab1645515c0c4
DJStefan <stefan.kussmaul@gmail.com>
2019-09-03 16:22:36 -0400

    Fixed featured carousel, added next/prev post links

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@ call venv\Scripts\activate
 python -m flask init-db
 echo y|python add_post.py C:\Users\Stefan\Github\Blog-Webcode\sample_post
 echo y|python add_post.py C:\Users\Stefan\Github\Blog-Content\project-overview-spaceships
+echo y|python add_post.py C:\Users\Stefan\Github\Blog-Content\project-overview-c++-game
 set FLASK_APP=flaskr
 set FLASK_ENV=development
 python -m flask run
