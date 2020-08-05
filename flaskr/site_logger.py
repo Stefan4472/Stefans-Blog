c16a7a9877b047423ea71c2ce79eb7e93281ffa6
DJStefan <stefan.kussmaul@gmail.com>
2020-08-04 22:21:17 -0400

    Updated to use the site-analytics for recording traffic (#18)

--- flaskr/site_logger.py
+++ flaskr/site_logger.py
@@ -2,13 +2,19 @@
 # space-separated: url timestamp IP-address
 import datetime
 from flask import request, current_app
+import requests
+
 
 def log_visit():
-    # Format current time, accurate to the milliseconds (see https://stackoverflow.com/a/18406412)
-    timestamp = datetime.datetime.now().strftime(r'%m-%d-%Y-%H:%M:%S:%f')[:-3]
-    url = request.path
-    # see https://stackoverflow.com/questions/3759981/get-ip-address-of-visitors-using-flask-for-python
-    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.environ['REMOTE_ADDR'])
-    user_agent = request.environ['HTTP_USER_AGENT']
-    with open(current_app.config['SITE_LOG_PATH'], 'a') as log_file:
-        log_file.write('{},{},{},{}\n'.format(timestamp, url, ip_addr, user_agent))
+    # Get request IP address: https://stackoverflow.com/questions/3759981/get-ip-address-of-visitors-using-flask-for-python
+    user_ip = request.environ.get(
+        'HTTP_X_FORWARDED_FOR', request.environ['REMOTE_ADDR']
+    )
+
+    # Send data to the site-traffic analyzer
+    params = {
+        'url': request.path, 
+        'ip_addr': user_ip,  
+        'user_agent': request.environ['HTTP_USER_AGENT'],
+    }
+    requests.post('http://sedu.pythonanywhere.com/report_traffic', params=params)
