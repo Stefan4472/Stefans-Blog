79d02b67e74d5315bf6ddb103a0ab17937364d12
DJStefan <stefan.kussmaul@gmail.com>
2022-08-03 13:58:32 +0200

    #61 transactional welcome email + template
    - Sending the welcome email is now done by the server (rather than depending on a SendInBlue trigger)
    - The email HTML is generated using a Jinja template

--- flaskr/email_provider.py
+++ flaskr/email_provider.py
@@ -1,7 +1,6 @@
-from flask import current_app
+from flask import current_app, render_template, url_for
 import sib_api_v3_sdk
 from sib_api_v3_sdk.rest import ApiException
-import time
 
 
 class EmailProvider:
@@ -19,17 +18,50 @@ class EmailProvider:
             return False
 
         print(f'Registering address {address}')
-        configuration = sib_api_v3_sdk.Configuration()
-        configuration.api_key['api-key'] = current_app.config['EMAIL_KEY']
-        api_instance = sib_api_v3_sdk.ContactsApi(sib_api_v3_sdk.ApiClient(configuration))
+        # configuration = sib_api_v3_sdk.Configuration()
+        # configuration.api_key['api-key'] = current_app.config['EMAIL_KEY']
+        # api_instance = sib_api_v3_sdk.ContactsApi(sib_api_v3_sdk.ApiClient(configuration))
 
         try:
-            api_instance.create_contact(sib_api_v3_sdk.CreateContact(
-                email=address,
-                update_enabled=True,
-                list_ids=[current_app.config['EMAIL_LIST_ID']],
-            ))
+            # api_instance.create_contact(sib_api_v3_sdk.CreateContact(
+            #     email=address,
+            #     update_enabled=True,
+            #     list_ids=[current_app.config['EMAIL_LIST_ID']],
+            # ))
+            self._send_welcome_email(address)
             return True
         except ApiException as e:
             print(f'Exception when calling ContactsApi->create_contact: {e}')
             return False
+
+    def _send_welcome_email(self, address: str):
+        """
+        Send a transactional "welcome" email to the specified address.
+
+        See https://developers.sendinblue.com/reference/sendtransacemail
+        """
+        configuration = sib_api_v3_sdk.Configuration()
+        configuration.api_key['api-key'] = current_app.config['EMAIL_KEY']
+        api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+
+        # Render the email in HTML
+        email_html = render_template(
+            'email/welcome_email.html',
+            # header_image=url_for('static', filename='site-banner.jpg', _external=True),
+            header_url='https://www.stefanonsoftware.com/static/site-banner.JPG',
+            recipient=address,
+        )
+
+        try:
+            api_response = api_instance.send_transac_email(sib_api_v3_sdk.SendSmtpEmail(
+                to=[{"email": address}],
+                reply_to={"email": "stefan@stefanonsoftware.com", "name": "Stefan Kussmaul"},
+                html_content=email_html,
+                sender={"name": "Stefan Kussmaul", "email": "stefan@stefanonsoftware.com"},
+                subject='Welcome to the StefanOnSoftware Email List!',
+            ))
+            print(api_response)
+        except ApiException as e:
+            print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
+
+    # def broadcast_email(self) -> bool:
