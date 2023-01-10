9d05872cea765ebb518fa661d3bd7fee78a5ed38
Stefan Kussmaul <stefan.kussmaul@gmail.com>
2023-01-10 21:59:21 +0100

    #78 update.py and manage.py

---
+++ Client/bin/manage.py
@@ -0,0 +1,220 @@
+"""CLI interface for several commonly-used management functions using the StefanOnSoftware API."""
+import click
+from typing import Optional
+from sos_client import client_util
+from stefan_on_software_api_client.types import UNSET, HTTPStatus
+from stefan_on_software_api_client.models.post_commands_publish_json_body import PostCommandsPublishJsonBody
+from stefan_on_software_api_client.models.post_commands_unpublish_json_body import PostCommandsUnpublishJsonBody
+from stefan_on_software_api_client.models.post_commands_feature_json_body import PostCommandsFeatureJsonBody
+from stefan_on_software_api_client.models.post_commands_unfeature_json_body import PostCommandsUnfeatureJsonBody
+from stefan_on_software_api_client.models.post_tags_json_body import PostTagsJsonBody
+from stefan_on_software_api_client.models.put_tags_tag_json_body import PutTagsTagJsonBody
+import stefan_on_software_api_client.api.commands.post_commands_publish as api_publish_post
+import stefan_on_software_api_client.api.commands.post_commands_unpublish as api_unpublish_post
+import stefan_on_software_api_client.api.commands.post_commands_feature as api_feature_post
+import stefan_on_software_api_client.api.commands.post_commands_unfeature as api_unfeature_post
+import stefan_on_software_api_client.api.posts.delete_posts_post_id as api_delete_post
+import stefan_on_software_api_client.api.tags.post_tags as api_create_tag
+import stefan_on_software_api_client.api.tags.put_tags_tag as api_update_tag
+import stefan_on_software_api_client.api.tags.delete_tags_tag as api_delete_tag
+
+
+@click.group()
+def cli():
+    pass
+
+
+# TODO: these commands will be executed by humans; should probably use post slugs instead
+# TODO: detailed readme with example commands
+@cli.command()
+@click.argument('post_id', type=int)
+@click.option('--send_email', required=True, type=bool)
+@click.option('--email', required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def publish_post(
+        post_id: int,
+        send_email: bool,
+        email: str,
+        password: str,
+        host_url: str,
+):
+    """
+    Publish a post.
+
+    POST_ID: ID of the post to publish.
+    SEND_EMAIL: Whether to trigger an email campaign to subscribers.
+    """
+    client = client_util.make_client(host_url, email, password)
+    res = api_publish_post.sync_detailed(
+        client=client,
+        json_body=PostCommandsPublishJsonBody(
+            post_id,
+            send_email,
+        ))
+    if res.status_code != HTTPStatus.NO_CONTENT:
+        click.echo(f'Request failed with content={res.content}')
+
+
+@cli.command()
+@click.argument('post_id', type=int)
+@click.option('--email', type=str, required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def unpublish_post(
+        post_id: int,
+        email: str,
+        password: str,
+        host_url: str,
+):
+    client = client_util.make_client(host_url, email, password)
+    res = api_unpublish_post.sync_detailed(
+        client=client,
+        json_body=PostCommandsUnpublishJsonBody(post_id),
+    )
+    if res.status_code != HTTPStatus.NO_CONTENT:
+        click.echo(f'Request failed with content={res.content}')
+
+
+@cli.command()
+@click.argument('post_id', type=int)
+@click.option('--email', type=str, required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def feature_post(
+        post_id: int,
+        email: str,
+        password: str,
+        host_url: str,
+):
+    client = client_util.make_client(host_url, email, password)
+    res = api_feature_post.sync_detailed(
+        client=client,
+        json_body=PostCommandsFeatureJsonBody(post_id),
+    )
+    if res.status_code != HTTPStatus.NO_CONTENT:
+        click.echo(f'Request failed with content={res.content}')
+
+
+@cli.command()
+@click.argument('post_id', type=int)
+@click.option('--email', type=str, required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def unfeature_post(
+        post_id: int,
+        email: str,
+        password: str,
+        host_url: str,
+):
+    client = client_util.make_client(host_url, email, password)
+    res = api_unfeature_post.sync_detailed(
+        client=client,
+        json_body=PostCommandsUnfeatureJsonBody(post_id),
+    )
+    if res.status_code != HTTPStatus.NO_CONTENT:
+        click.echo(f'Request failed with content={res.content}')
+
+
+@cli.command()
+@click.argument('post_id', type=int)
+@click.option('--email', type=str, required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def delete_post(
+        post_id: int,
+        email: str,
+        password: str,
+        host_url: str,
+):
+    client = client_util.make_client(host_url, email, password)
+    res = api_delete_post.sync_detailed(
+        post_id,
+        client=client,
+    )
+    if res.status_code != HTTPStatus.NO_CONTENT:
+        click.echo(f'Request failed with content={res.content}')
+
+
+@cli.command()
+@click.argument('slug', type=str, required=True)
+@click.argument('name', type=str, required=True)
+@click.argument('description', type=str, required=True)
+@click.option('--color', type=str)
+@click.option('--email', type=str, required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def create_tag(
+        slug: str,
+        name: str,
+        description: str,
+        color: Optional[str],
+        email: str,
+        password: str,
+        host_url: str,
+):
+    client = client_util.make_client(host_url, email, password)
+    res = api_create_tag.sync_detailed(
+        client=client,
+        json_body=PostTagsJsonBody(
+            name,
+            slug,
+            description,
+            color=color if color else UNSET,
+        ))
+    if res.status_code != HTTPStatus.CREATED:
+        click.echo(f'Request failed with content={res.content}')
+
+
+@cli.command()
+@click.argument('slug', type=str)
+@click.argument('name', type=str)
+@click.argument('description', type=str)
+@click.argument('color', type=str)
+@click.option('--email', type=str, required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def update_tag(
+        slug: str,
+        name: str,
+        description: str,
+        color: str,
+        email: str,
+        password: str,
+        host_url: str,
+):
+    client = client_util.make_client(host_url, email, password)
+    res = api_update_tag.sync_detailed(
+        slug,
+        client=client,
+        json_body=PutTagsTagJsonBody(
+            name,
+            description,
+            color,
+        ))
+    if res.status_code != HTTPStatus.OK:
+        click.echo(f'Request failed with content={res.content}')
+
+
+@cli.command()
+@click.argument('slug', type=str)
+@click.option('--email', type=str, required=True, help='Email address used to authenticate with the API')
+@click.password_option(required=True)
+@click.option('--host_url', required=True, help='Host URL of the site instance, e.g. http://localhost:5000')
+def delete_tag(
+        slug: str,
+        email: str,
+        password: str,
+        host_url: str,
+):
+    client = client_util.make_client(host_url, email, password)
+    res = api_delete_tag.sync_detailed(
+        tag=slug,
+        client=client,
+    )
+    if res.status_code != HTTPStatus.NO_CONTENT:
+        click.echo(f'Request failed with content={res.content}')
+
+
+if __name__ == '__main__':
+    cli()
---
+++ Client/bin/update.py
@@ -0,0 +1,95 @@
+import click
+import sys
+from pathlib import Path
+import renderer.markdown
+from sos_client import client_util
+from stefan_on_software_api_client.types import File as UploadFile, UNSET, HTTPStatus
+from stefan_on_software_api_client.models.post_posts_post_id_content_multipart_data import PostPostsPostIdContentMultipartData
+from stefan_on_software_api_client.models.put_posts_post_id_json_body import PutPostsPostIdJsonBody
+import stefan_on_software_api_client.api.posts.post_posts_post_id_content as api_set_post_content
+import stefan_on_software_api_client.api.posts.put_posts_post_id as api_update_post
+
+
+# TODO: needs significant revisions and error-handling.
+# TODO: repeated code with upload.py
+@click.command()
+@click.argument('post_id', type=int)
+@click.argument('path', type=click.Path(exists=True, dir_okay=True, file_okay=False, path_type=Path))
+@click.option('--email', required=True)
+@click.password_option(required=True)
+@click.option('--host_url', required=True)
+def update_post(post_id: int, path: Path, email: str, password: str, host_url: str):
+    """
+    Update the Markdown (and upload all images) for a given post.
+
+    POST_ID: the ID of the post to update.
+    """
+    try:
+        post = client_util.load_post(path)
+    except ValueError as e:
+        click.echo(e, err=True)
+        sys.exit(1)
+
+    client = client_util.make_client(host_url, email, password)
+
+    # TODO: should file upload reject duplicates explicitly?
+    featured_path = (path / post.metadata.image).resolve()
+    click.echo(f'Uploading featured image {featured_path}...')
+    with open(featured_path, mode='rb') as contents:
+        featured_id = client_util.upload_file(client, UploadFile(contents, featured_path.name)).id
+
+    banner_path = (path / post.metadata.banner).resolve()
+    click.echo(f'Uploading banner image {banner_path}...')
+    with open(banner_path, mode='rb') as contents:
+        banner_id = client_util.upload_file(client, UploadFile(contents, banner_path.name)).id
+
+    thumbnail_path = (path / post.metadata.thumbnail).resolve()
+    click.echo(f'Uploading thumbnail image {thumbnail_path}...')
+    with open(thumbnail_path, mode='rb') as contents:
+        thumbnail_id = client_util.upload_file(client, UploadFile(contents, thumbnail_path.name)).id
+
+    click.echo('Updating post...')
+    res_update = api_update_post.sync_detailed(
+        post_id,
+        client=client,
+        json_body=PutPostsPostIdJsonBody(
+            slug=post.metadata.slug if post.metadata.slug else UNSET,
+            title=post.metadata.title if post.metadata.title else UNSET,
+            byline=post.metadata.byline if post.metadata.byline else UNSET,
+            featured_image=featured_id if featured_id else UNSET,
+            banner_image=banner_id if banner_id else UNSET,
+            thumbnail_image=thumbnail_id if thumbnail_id else UNSET,
+        ),
+    )
+    print(res_update.status_code)
+    if res_update.status_code != HTTPStatus.OK:
+        click.echo(f'Request failed with content={res_update.content}', err=True)
+        sys.exit(1)
+
+    with open(post.md_path) as markdown_file:
+        post_md = markdown_file.read()
+    # Get the list of image filenames referenced in the Markdown
+    for filename in renderer.markdown.find_images(post_md):
+        # Resolve absolute path
+        full_path = (path / filename).resolve()
+        # Upload image and get its online filename
+        click.echo(f'Uploading image {full_path}...')
+        with open(full_path, mode='rb') as contents:
+            # TODO: not sure if this will work correctly
+            new_filename = client_util.upload_file(client, UploadFile(contents, full_path.name)).upload_name
+            # Update Markdown to use the new filename TODO: the naive find-and-replace is risky!
+            post_md = post_md.replace(filename, new_filename)
+
+    click.echo('Uploading Markdown...')
+    res_content = api_set_post_content.sync_detailed(
+        post_id,
+        client=client,
+        multipart_data=PostPostsPostIdContentMultipartData(UploadFile(post_md, 'post.md')),
+    )
+    if res_content.status_code != HTTPStatus.NO_CONTENT:
+        click.echo(f'Request failed with content={res_content.content}')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    update_post()
