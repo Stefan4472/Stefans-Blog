9668b7beab728e2420d3584b0f01ec183ed4822b
Stefan Kussmaul <stefan.kussmaul@gmail.com>
2023-04-03 21:47:56 +0200

    #84 fix bug affecting requests that don't have a user agent
    `site_logger` was crashing because occasional requests don't have a user agent, causing `request.environ['HTTP_USER_AGENT']` to crash.

--- stefan-on-software/stefan_on_software/site_logger.py
+++ stefan-on-software/stefan_on_software/site_logger.py
@@ -10,16 +10,16 @@ from .site_config import ConfigKeys
 
 def log_visit():
     """Logs a visit to local file and notifies traffic API, if configured."""
-    # Log to local file
     timestamp = datetime.datetime.now().strftime(r"%m-%d-%Y-%H:%M:%S:%f")[:-3]
     url = request.path
-    # see https://stackoverflow.com/questions/3759981/get-ip-address-of-visitors-using-flask-for-python
-    ip_addr = request.environ.get(
+    # https://stackoverflow.com/questions/3759981/get-ip-address-of-visitors-using-flask-for-python
+    ip_address = request.environ.get(
         "HTTP_X_FORWARDED_FOR", request.environ["REMOTE_ADDR"]
     )
-    user_agent = request.environ["HTTP_USER_AGENT"]
+    user_agent = request.environ.get("HTTP_USER_AGENT", "")
+
     with open(current_app.config[ConfigKeys.TRAFFIC_LOG_PATH], "a") as log_file:
-        log_file.write("{},{},{},{}\n".format(timestamp, url, ip_addr, user_agent))
+        log_file.write(f"{timestamp},{url},{ip_address},{user_agent}\n")
 
     if current_app.config[ConfigKeys.USE_SITE_ANALYTICS]:
         # Send data to site-analytics
@@ -28,7 +28,7 @@ def log_visit():
                 current_app.config[ConfigKeys.SITE_ANALYTICS_URL],
                 params={
                     "url": request.path,
-                    "ip_addr": ip_addr,
+                    "ip_addr": ip_address,
                     "user_agent": request.environ["HTTP_USER_AGENT"],
                     "secret": current_app.config[ConfigKeys.SITE_ANALYTICS_KEY],
                 },
