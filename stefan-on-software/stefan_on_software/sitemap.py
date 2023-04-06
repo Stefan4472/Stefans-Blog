cdc9a008182a4c53d0527220b9c496ce66d56323
Stefan Kussmaul <stefan.kussmaul@gmail.com>
2023-04-06 21:34:18 +0200

    #100 ping Google when sitemap changes

--- stefan-on-software/stefan_on_software/sitemap.py
+++ stefan-on-software/stefan_on_software/sitemap.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Iterable
 
 import flask
+import requests
 from stefan_on_software.models.post import Post
 from stefan_on_software.models.tag import Tag
 from stefan_on_software.site_config import ConfigKeys
@@ -23,11 +24,26 @@ def generate_and_write_sitemap(output_path: Path):
     """
     with open(output_path, "w+", encoding="utf-8") as out:
         out.write(generate_sitemap(Post.query.all(), Tag.query.all()))
+    flask.current_app.logger.info(f"Updated and wrote sitemap.")
+
+
+def ping_google():
+    """
+    Send Google a ping which tells them our sitemap has changed.
+    See https://developers.google.com/search/docs/crawling-indexing/sitemaps/build-sitemap#addsitemap.
+    """
+    requests.get(
+        f"https://www.google.com/ping?sitemap={flask.url_for('blog.sitemap', _external=True)}"
+    )
+    flask.current_app.logger.info("Notified Google about sitemap change.")
 
 
 def update_sitemap():
     """
     Dynamically generates the sitemap and writes it to the path defined
     in the site config. Will create the sitemap if it doesn't exist yet.
+    Will notify Google about the change if TESTING=false.
     """
     generate_and_write_sitemap(Path(flask.current_app.config[ConfigKeys.SITEMAP_PATH]))
+    if not flask.current_app.config[ConfigKeys.TESTING]:
+        ping_google()
